3c53c3cbaa8e01c1a5cbedc2ec6fbc958848702e
Oliver Schmitz <o.schmitz@uu.nl>
2023-09-25 10:05:20 +0200

    Build fix Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,8 @@ cmake ${CMAKE_ARGS} -S osmium-tool \
  -B ${BUILD_DIR} \
  -G "Ninja" \
  -D CMAKE_BUILD_TYPE=Release \
+ -D OSMIUM_INCLUDE_DIR=libosmium/include \
+ -D PROTOZERO_INCLUDE_DIR=protozero/include \
  -D CMAKE_INSTALL_PREFIX="${PREFIX}"
 
 cmake --build ${BUILD_DIR} --target all
